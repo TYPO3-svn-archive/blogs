b8494defa5fc3408efd44d0ce6d0a5d3ad381db0
jesper_paardekooper <jesper_paardekooper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-24 18:05:57 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/blogs/trunk@16055 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "blogs"
+#
+# Auto generated 24-01-2009 19:04
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '',
+	'description' => '',
+	'category' => '',
+	'author' => '',
+	'author_email' => '',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => '',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"66fe";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"9cbe";s:14:"ext_tables.php";s:4:"7c7e";s:14:"ext_tables.sql";s:4:"90d5";s:28:"flexform_ds_pi1_settings.xml";s:4:"85c7";s:28:"icon_tx_blogs_categories.gif";s:4:"355b";s:26:"icon_tx_blogs_comments.gif";s:4:"4ad7";s:23:"icon_tx_blogs_items.gif";s:4:"a25c";s:16:"locallang_db.xml";s:4:"b299";s:7:"tca.php";s:4:"adf2";s:19:"doc/wizard_form.dat";s:4:"e93d";s:20:"doc/wizard_form.html";s:4:"63a1";s:26:"pi1/class.tx_blogs_pi1.php";s:4:"8ddf";s:17:"pi1/locallang.xml";s:4:"2789";s:11:"pi1/rss.gif";s:4:"c830";s:17:"pi1/template.html";s:4:"9d27";s:26:"static/blogs/constants.txt";s:4:"2309";s:22:"static/blogs/setup.txt";s:4:"627d";}',
+	'suggests' => array(
+	),
+);
+
+?>
